80f54d6cd6c57df8127da3f134308a062b4caaba
obloomfield <obloomfield@brown.edu>
2023-05-07 20:04:16 -0400

    mcts greedy stencil

--- src/convex-hull/mcts.h
+++ src/convex-hull/mcts.h
@@ -7,5 +7,7 @@ class MCTS
 public:
     MCTS();
 
-    static quickhull::Plane<double> cuttingPlane(Mesh cur_mesh);
+    static quickhull::Plane<double> cuttingPlane(const Mesh& cur_mesh);
+
+    static quickhull::Plane<double> cuttingPlaneGreedy(const Mesh& cur_mesh);
 };
